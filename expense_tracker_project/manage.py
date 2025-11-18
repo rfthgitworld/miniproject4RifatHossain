7305ce31880d358260768244d2c7e37cc075d02b
Rifat Hossain <r_hossain@mail.fhsu.edu>
2025-11-17 20:54:41 -0500

    Added project requirements as commnets in the manage.py

--- expense_tracker_project/manage.py
+++ expense_tracker_project/manage.py
@@ -1,3 +1,30 @@
+# INF601 - Advanced Programming in Python
+# Rifat Hossain
+# Mini Project 4 - Django Web Application
+
+
+
+# This project will be using Django to deploy a small web app of your choice. The goal here is to come up with a small web application that meets the requirements below. If you get stuck here, please email me!
+#
+# (5/5 points) Initial comments with your name, class and project at the top of your .py file.
+# (5/5 points) Proper import of packages used.
+# (70/70 points) Using Django you need to setup the following:
+# (10/10 points) Setup a proper folder structure, use the tutorial as an example. You need a minimum of one app.
+# (20/20 points) You need to have a minimum of 5 pages, using a proper template structure.
+# (10/10 points) You need to have at least one page that utilizes a form.
+# (10/10 points) You need to setup Django admin and style your models for proper entry. No tight restrictions here, just make use of the admin system.
+# (10/10) You need to use Bootstrap in your web templates. I won't dictate exactly what modules you need to use but the more practice here the better. You need to at least make use of a modal.
+# (10/10) You need to setup some sort of register and login system, do some simple research to find examples.
+# (5/5 points) There should be a minimum of 5 commits on your project, be sure to commit often!
+# (5/5 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
+# (10/10 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations. You will need to explain the steps of initializing the database and then how to run the development server for your project.
+# Remember to run these commands and have them in your README:
+#
+# python manage.py makemigrations (this will create any SQL entries that need to go into the database)
+# python manage.py migrate (this will apply the migrations)
+# python manage.py createsuperuser (this will create the administrator login for your /admin side of your project)
+
+
 #!/usr/bin/env python
 import os
 import sys
